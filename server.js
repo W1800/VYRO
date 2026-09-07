a91d90bfd4dceb6d8a53d61086caced25d02982f
W1800 <150682309+W1800@users.noreply.github.com>
2026-09-07 09:13:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
-const app = express();
+const app = express();const cors = require("cors");
+app.use(cors());
 const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
