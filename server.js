eb61f771c4e31907364e6ac3b6a57fd39fe1fe18
W1800 <150682309+W1800@users.noreply.github.com>
2026-09-07 08:18:56 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.get("/", (req, res) => {
+  res.send("VYRO Backend is running!");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
